29a9dfc8be87ba049c5840a0ed54bef693473002
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-02-13 18:58:48 -0500

    re

--- Ian Meany Bird/Assets/Scripts/Collectibles.cs
+++ Ian Meany Bird/Assets/Scripts/Collectibles.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectibles : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ //If an object collides with a trigger
+    private void onTriggerEnter2D(Collider2D collision)
+    
     {
-
+        //Add score
+        Score.score++;
     }
 
     // Update is called once per frame
--- Ian Meany Bird/Assets/Scripts/PlayerControls.cs
+++ Ian Meany Bird/Assets/Scripts/PlayerControls.cs
@@ -35,4 +35,12 @@ public class PlayerControls : MonoBehaviour
             rb.velocity = Vector2.up * velocity;
         }
     }
+//Function where the player collides with a object
+private void OnCollisionEnter2D(Collision2D collision)
+{
+    if (collision.gameObject.tag == "HighSpike" || collision.gameObject.tag == "LowSpike" || collision.gameObject.tag == "Ground")
+        //Game is at a stopping state
+        Time.timeScale = 0;
 }
+}
+
