6ef7a48744bccb3aec2872adb754966554fa35b5
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-04-02 17:31:32 -0400

    s

--- Ian's Don't Touch The Cubes/Assets/Scripts/GameControls.cs
+++ Ian's Don't Touch The Cubes/Assets/Scripts/GameControls.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameControls: MonoBehaviour
-{
+{   //Timer text object
+    private Text timerText;
+    //Timer counter for adding score
+    private int timerCount;
     // Start is called before the first frame update
     void Start()
     {
-
+        //Game is at a playing state
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(0);
     }
 }
