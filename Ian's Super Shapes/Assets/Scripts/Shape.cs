0a8e85318828c00773462627bf69f988e3367157
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-05-07 17:30:18 -0400

    fg

--- Ian's Super Shapes/Assets/Scripts/Shape.cs
+++ Ian's Super Shapes/Assets/Scripts/Shape.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shape : MonoBehaviour
-{
+{   //Rigidbody for object
+    [Header("Rigidbody Object")]
+    public Rigidbody2D rb;
+    //Shrinking speed
+    [Header("Default Shrinking Speed")]
+    public float shrinkSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
+        //Rotation of the rigidbody
 
     }
 
